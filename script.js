0d67343b5ab47b1cd6a0c64e78d367bbcdb03ed6
Mau1743 <103154413+Mau1743@users.noreply.github.com>
2022-04-25 19:09:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,265 @@
+function iniciar(){
+    var imagenes=document.querySelectorAll('#cajaimagenes img');
+     soltar=document.getElementById('cajasoltar');  
+     soltar2  =document.getElementById('cajasoltar2'); 
+     soltar3 = document.getElementById('cajasoltar3'); 
+
+    for(var i=0; i<imagenes.length; i++){
+    imagenes[i].addEventListener('dragstart', arrastrado, false);
+    imagenes[i].addEventListener('dragend', finalizado, false);
+    }
+
+    soltar.addEventListener('dragenter', function(e){
+    e.preventDefault(); }, false);
+    soltar.addEventListener('dragover', function(e){
+    e.preventDefault(); }, false);
+    soltar.addEventListener('drop', soltado, false);
+
+
+    soltar2.addEventListener('dragenter', function(e){
+        e.preventDefault(); }, false);
+    soltar2.addEventListener('dragover', function(e){
+    e.preventDefault(); }, false);
+    soltar2.addEventListener('drop', soltado2, false);
+
+
+    soltar3.addEventListener('dragenter', function(e){
+        e.preventDefault(); }, false);
+    soltar3.addEventListener('dragover', function(e){
+    e.preventDefault(); }, false);
+    soltar3.addEventListener('drop', soltado3, false);
+
+}
+function arrastrado(e){
+    elemento=e.target;
+    e.dataTransfer.setData('Text', elemento.getAttribute('id'));
+}
+function finalizado(e){
+    elemento=e.target;
+    elemento.style.visibility='hidden';
+} 
+
+function soltado(e){
+    e.preventDefault();
+    var id=e.dataTransfer.getData('Text');
+    if(id!="imagen4"){
+        var src=document.getElementById(id).src;
+    soltar.innerHTML='<img src="'+src+'" height="400px" width="275px">';
+    }else{
+    soltar.innerHTML='la imagen no es admitida';
+    }
+}
+
+function soltado2(e){
+    e.preventDefault();
+    var id=e.dataTransfer.getData('Text');
+    if(id!="imagen4"){
+        var src=document.getElementById(id).src;
+    soltar2.innerHTML='<img src="'+src+'"height="400px" width="275px">';
+    }else{
+    soltar2.innerHTML='la imagen no es admitida';
+    }
+}
+function soltado3(e){
+    e.preventDefault();
+    var id=e.dataTransfer.getData('Text');
+    if(id!="imagen4"){
+        var src=document.getElementById(id).src;
+    soltar3.innerHTML='<img src="'+src+'"height="400px" width="275px">';
+    }else{
+    soltar3.innerHTML='la imagen no es admitida';
+    }
+}
+
+function reinicio() {
+    window.location.reload();
+}
+iniciar()
+
+
+
+
+
+
+
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+const currTime = document.querySelector('#currTime');
+const durTime = document.querySelector('#durTime');
+
+// Song titles
+const songs = ['Best of me','Cold','Fight Back'];
+
+
+// Keep track of song
+let songIndex = 0;
+
+// Initially load song details into DOM
+loadSong(songs[songIndex]);
+
+// Update song details
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+  
+}
+
+
+// Play song
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+  audio.play();
+}
+
+// Pause song
+function pauseSong() {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+  audio.pause();
+}
+
+// Previous song
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+// Next song
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+// Update progress bar
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+// Set progress bar
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
+//get duration & currentTime for Time of song
+function DurTime (e) {
+	const {duration,currentTime} = e.srcElement;
+	var sec;
+	var sec_d;
+
+	// define minutes currentTime
+	let min = (currentTime==null)? 0:
+	 Math.floor(currentTime/60);
+	 min = min <10 ? '0'+min:min;
+
+	// define seconds currentTime
+	function get_sec (x) {
+		if(Math.floor(x) >= 60){
+			
+			for (var i = 1; i<=60; i++){
+				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+					sec = Math.floor(x) - (60*i);
+					sec = sec <10 ? '0'+sec:sec;
+				}
+			}
+		}else{
+		 	sec = Math.floor(x);
+		 	sec = sec <10 ? '0'+sec:sec;
+		 }
+	} 
+
+	get_sec (currentTime,sec);
+
+	// change currentTime DOM
+	currTime.innerHTML = min +':'+ sec;
+
+	// define minutes duration
+	let min_d = (isNaN(duration) === true)? '0':
+		Math.floor(duration/60);
+	 min_d = min_d <10 ? '0'+min_d:min_d;
+
+
+	 function get_sec_d (x) {
+		if(Math.floor(x) >= 60){
+			
+			for (var i = 1; i<=60; i++){
+				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+					sec_d = Math.floor(x) - (60*i);
+					sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+				}
+			}
+		}else{
+		 	sec_d = (isNaN(duration) === true)? '0':
+		 	Math.floor(x);
+		 	sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+		 }
+	} 
+
+	// define seconds duration
+	
+	get_sec_d (duration);
+
+	// change duration DOM
+	durTime.innerHTML = min_d +':'+ sec_d;
+		
+};
+
+// Event listeners
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+// Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// Time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
+
+// Time of song
+audio.addEventListener('timeupdate',DurTime);
